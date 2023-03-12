ce29754805ce7b486c8430d45bca5fdcaefa3338
anicktoo <anickt00@gmail.com>
2023-03-12 06:14:43 +0300

    header and sidebar css, module-integrator, modules

---
+++ main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************!*\
+  !*** ./app.js ***!
+  \****************/
+// import '@styles/styles.scss';
+// import moduleIntegration from './module-integration';
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
