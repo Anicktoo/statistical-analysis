b5025e0d1eea3c64df804249dcdb838808820642
anicktoo <anickt00@gmail.com>
2023-05-11 22:28:08 +0300

    .

--- src/app.js
+++ src/app.js
@@ -2,11 +2,6 @@ import '@styles/styles.scss';
 import '@/utils/utils';
 import '@/uiControls';
 import moduleIntegrator from '@/moduleIntegrator';
-import { binTest } from './tests';
 
 uiControls.initConstuiControls();
 moduleIntegrator.createModuleButtons();
-
-console.log('Test');
-
-binTest();
